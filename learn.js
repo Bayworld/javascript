a28d4c48dbb6cd9073224ebcd70596846c274be2
Bayworld <abayomikiel@gmail.com>
2021-01-13 23:36:45 +0100

    'learn'

---
+++ learn.js
@@ -0,0 +1,31 @@
+// // function isEven(element){
+// //     if(element % 2 === 0){
+// //         return true;
+// //     }
+// //     return false;
+// // }
+// // console.log(isEven(12));
+
+// const textArray = [2, 3 ,4 , 5 , 1 ,7 ,9 , 8];
+
+// //console.log(textArray.fill("empty"));
+
+// const result = [23, 34 ,56 ,67, 68 ,89 ,90, 95];
+
+// //console.log(result.filter((num) => num != 56));
+
+// //const user = ['tom', 'tery', 'feng', 'tunner', 'sunny', 'sone'];
+// //console.log(user.slice(1, 2));
+
+// const user = [ "renny", "diyu", "turry", "furty", "reany", "denny"]
+// console.log(user.splice(1,2));
+
+const users = {
+    firstName : 'Abayomi',
+    secondName : 'Agboola',
+    role : 'Admin',
+    loginCount : 32,
+    facebookSignedIn : true
+};
+console.log(users.firstName);
+
