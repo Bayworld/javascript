08e2ae82abaab1549dec08b149bf2a5dda3e3a45
Bayworld <abayomikiel@gmail.com>
2021-01-14 18:34:43 +0100

    'refresh'

--- learn.js
+++ learn.js
@@ -1,9 +1,31 @@
-const rainbow =
-['gray','orange','cream','green','blue','rebeccapurple','violet', 'gold'];
-setInterval(
-function change() {
-document.body.style.background =
- rainbow[Math.floor(7*Math.random())];
-},2000);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20)
+            $(".navbar").addClass("sticky")
+        else
+        $(".navbar").removeClass("sticky")
+    });
+
+    $(".menu-toggler").click(function(){
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
